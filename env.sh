7b74b598d5bd62811e1460c607d95af8fef81abe
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-09-11 10:55:48 +0100

    Fix python env

--- env.sh
+++ env.sh
@@ -15,3 +15,9 @@ fi
 
 # Source Homebrew, the rest is story
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# Fix some madeness about Python3.10 being the default in Ubuntu:22.04 and 3.11 in Homebrew
+# FIXME: This is probably the only weak point of this setup. And there is a hardcoded python vesion,
+# in this env, which sucks :)
+export PATH="$HOMEBREW_PREFIX/opt/python@3.10/libexec/bin:$PATH"
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python3/dist-packages/
